aad81bb2b297c8d995d6f8c2c28e11e3f19ce42a
Kayo Weiber <caioveiber598@gmail.com>
2025-11-05 10:45:55 -0300

    feat: criado o footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Historia from './components/Historia';
 import Galeria from './components/Galeria';
 import NotFound from './components/NotFound';
@@ -15,6 +16,7 @@ function App() {
             <Route path="/galeria" element={<Galeria />} />
               <Route path="*" element={<NotFound />} />
           </Routes>
+          <Footer />
         </BrowserRouter>
     </>
   )
